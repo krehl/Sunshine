7d52fc63c657b347f15482ff732a0d92a627a35b
krehl <konstantin.krehl@gmail.com>
2015-07-26 21:59:15 +0200

    Implemented update method

--- app/src/main/java/de/kkrehl/udacity/sunshine/data/WeatherProvider.java
+++ app/src/main/java/de/kkrehl/udacity/sunshine/data/WeatherProvider.java
@@ -268,7 +268,7 @@ public class WeatherProvider extends ContentProvider {
                 rowsDeleted = db.delete(WeatherContract.LocationEntry.TABLE_NAME, selection, selectionArgs);
                 break;
             default:
-                throw new UnsupportedOperationException("Delete uri does not match known type.");
+                throw new UnsupportedOperationException("Delete uri does not match known type. " + uri);
         }
         // Student: A null value deletes all rows.  In my implementation of this, I only notified
         // the uri listeners (using the content resolver) if the rowsDeleted != 0 or the selection
@@ -294,7 +294,30 @@ public class WeatherProvider extends ContentProvider {
             Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         // Student: This is a lot like the delete function.  We return the number of rows impacted
         // by the update.
-        return 0;
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        // Student: Use the uriMatcher to match the WEATHER and LOCATION URI's we are going to
+        // handle.  If it doesn't match these, throw an UnsupportedOperationException.
+        int rowsUpdated;
+        switch (match) {
+            case WEATHER:
+                rowsUpdated = db.update(WeatherContract.WeatherEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            case LOCATION:
+                rowsUpdated = db.update(WeatherContract.LocationEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Delete uri does not match known type. " + uri);
+        }
+        // Student: A null value deletes all rows.  In my implementation of this, I only notified
+        // the uri listeners (using the content resolver) if the rowsDeleted != 0 or the selection
+        // is null.
+        // Oh, and you should notify the listeners here.
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        // Student: return the actual rows updated
+        return rowsUpdated;
     }
 
     @Override
